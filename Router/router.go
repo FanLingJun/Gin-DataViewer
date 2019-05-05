0f6ef6ebfa65a6d84e7de2c3ee63a64599779c03
FanLingJun <675546066@qq.com>
2019-05-05 13:24:15 +0800

    调整

--- Router/router.go
+++ Router/router.go
@@ -21,9 +21,7 @@ func RouterInit(router *gin.Engine) {
   if err != nil {
     fmt.Print(err)
   }
-  //fmt.Println(b)
   str := string(b)
-  // fmt.Println(str)
   router.GET("/data",func(c *gin.Context) {
     c.String(http.StatusOK,"%s",str)
   })
@@ -32,12 +30,32 @@ func RouterInit(router *gin.Engine) {
   if err != nil {
     fmt.Print(err)
   }
-  fmt.Println(a)
   str_all := string(a)
   // fmt.Println(str)
   router.GET("/alldata",func(c *gin.Context) {
     c.String(http.StatusOK,"%s",str_all)
   })
+
+  c, err := ioutil.ReadFile("re.json")
+  if err != nil {
+    fmt.Print(err)
+  }
+  str_word_cloud := string(c)
+  // fmt.Println(str)
+  router.GET("/wordcloudData",func(c *gin.Context) {
+    c.String(http.StatusOK,"%s",str_word_cloud)
+  })
+
+  d, err := ioutil.ReadFile("sales.json")
+  if err != nil {
+    fmt.Print(err)
+  }
+  str_sale_number := string(d)
+  // fmt.Println(str)
+  router.GET("/saleNumber",func(c *gin.Context) {
+    c.String(http.StatusOK,"%s",str_sale_number)
+  })
+
 
 
 }
